76ba99abfddfc730a2aefa1bbee043c98c5b1bee
Ruderson Florentino <rudersonvf@gmail.com>
2023-05-24 17:44:25 -0300

    First backend requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import ProductDetails from './routes/ProductDetails';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Catalog from '../src/routes/ClientHome/Catalog';
+import ProductDetails from '../src/routes/ClientHome/ProductDetails';
+import ClientHome from './routes/ClientHome';
 
 function App() {
   return (
-    <ProductDetails/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ClientHome />}>
+          <Route index element={<Catalog />} />
+          <Route path='catalog' element={<Catalog />} />
+          <Route path='product-details/:productId' element={<ProductDetails />} />
+        </Route>
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
