29dcc5c9b15c8dec90c98e034ef02c30198becba
Ruderson Florentino <rudersonvf@gmail.com>
2023-07-16 12:28:41 -0300

    searchbar onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,11 +1,47 @@
-import './styles.css'
+import { useState } from "react";
+import "./styles.css";
 
-export default function SearchBar() {
-    return (
-        <form className="dsc-search-bar">
-            <button type="submit">🔎︎</button>
-            <input type="text" placeholder="Nome do produto" />
-            <button type="reset">x</button>
-        </form>
-    );
+/*
+ * O evento é uma Prop do tipo function com "F" maiúsculo
+ */
+
+type Props = {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  onSearch: Function;
+};
+
+export default function SearchBar({ onSearch }: Props) {
+  const [text, setText] = useState("");
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    onSearch(text);
+  }
+
+  function handleChange(event: any) {
+    setText(event.target.value);
+  }
+
+  function handleResetClick(){
+    setText("");
+    onSearch(text);
+  }
+
+  /*
+   * Sempre que o formulário for enviado, é executada a função onSearch passando como argumento
+   * o valor contido na variavel text do useState, que por sua vez é o handleSearch do componente Catalog
+   */
+
+  return (
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
+      <button type="submit">🔎︎</button>
+      <input
+        value={text}
+        type="text"
+        placeholder="Nome do produto"
+        onChange={handleChange}
+      />
+      <button onClick={handleResetClick}>x</button>
+    </form>
+  );
 }
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -9,17 +9,30 @@ import { ProductDTO } from "../../../models/product";
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
+  const [prodName, setProdName] = useState("");
 
   useEffect(() => {
-    productService.findAll().then((response) => {
+    productService.findPageRequest(0, prodName).then((response) => {
       setProducts(response.data.content);
     });
-  }, []);
+  }, [prodName]); //<- Lista de dependẽncias do useEffect. Sempre que o valor mudar o useEffect executa a função novamente
 
-  return (  
+  function handleSearch(searchText: string) {
+    setProdName(searchText);
+  }
+
+  /* O componente catalogo vai observar o evento onSearch do componente SearchBar
+   * que quando for executada, vai executar a funcao handleSearch "lançar um evento"
+   * que por sua vez vai atualizar o prodName usando de argumento o resultado da função
+   * onSearch do componente SearchBar, que por sua vez vai disparar o useEffect que está observando
+   * o prodName nas dependências que irá executar novamente a função.
+   * Ver o componente SearchBar para saber mais sobre o evento onSearch.
+   */
+
+  return (
     <main>
       <section id="catalog-section" className="dsc-container">
-        <SearchBar />
+        <SearchBar onSearch={handleSearch} />
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {products.map((product) => (
