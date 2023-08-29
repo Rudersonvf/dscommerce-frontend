248f3dad3b2638b1c91cc34a2a4d1a2ae25a93bf
Ruderson Florentino <rudersonvf@gmail.com>
2023-08-28 22:15:39 -0300

    Product CRUD static layout

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,101 +1,61 @@
 import "./styles.css";
-import { useEffect, useState } from "react";
-import { ProductDTO } from "../../../models/product";
-import SearchBar from "../../../components/SearchBar";
-import * as productService from "../../../services/product-service";
-import deleteImg from "../../../assets/deletar.svg";
-import editImg from "../../../assets/editar.svg";
-
-type QueryParams = {
-  page: number;
-  name: string;
-};
 
 export default function ProductForm() {
-  const [isLastPage, setIsLastPage] = useState(false);
-  const [products, setProducts] = useState<ProductDTO[]>([]);
-  const [queryParams, setQueryParams] = useState<QueryParams>({
-    page: 0,
-    name: "",
-  });
-
-  useEffect(() => {
-    productService
-      .findPageRequest(queryParams.page, queryParams.name)
-      .then((response) => {
-        const nextPage = response.data.content;
-        setProducts(products.concat(nextPage));
-        setIsLastPage(response.data.last);
-      });
-  }, [queryParams]);
-
-  function handleSearch(searchText: string) {
-    setProducts([]);
-    setQueryParams({ ...queryParams, page: 0, name: searchText });
-  }
-
-  function handleNextPageClick() {
-    setQueryParams({ ...queryParams, page: queryParams.page + 1 });
-  }
-
   return (
     <main>
-      <section id="product-listing-section" className="dsc-container">
-        <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
+      <section id="product-form-section" className="dsc-container">
+        <div className="dsc-product-form-container">
+          <form className="dsc-card dsc-form">
+            <h2>Dados do produto</h2>
+            <div className="dsc-form-controls-container">
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Nome"
+                />
+              </div>
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Preço"
+                />
+              </div>
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Imagem"
+                />
+              </div>
+              <div>
+                <select className="dsc-form-control dsc-select" required>
+                  <option value="" disabled selected>
+                    Categorias
+                  </option>
+                  <option value="1">Valor 1</option>
+                  <option value="2">Valor 2</option>
+                </select>
+              </div>
+              <div>
+                <textarea
+                  className="dsc-form-control dsc-textarea"
+                  placeholder="Descrição"
+                ></textarea>
+              </div>
+            </div>
 
-        <div className="dsc-btn-page-container dsc-mb20">
-          <div className="dsc-btn dsc-btn-white">Novo</div>
+            <div className="dsc-product-form-buttons">
+              <button type="reset" className="dsc-btn dsc-btn-white">
+                Cancelar
+              </button>
+              <button type="submit" className="dsc-btn dsc-btn-blue">
+                Salvar
+              </button>
+            </div>
+          </form>
         </div>
-
-        <SearchBar onSearch={handleSearch} />
-
-        <table className="dsc-table dsc-mb20 dsc-mt20">
-          <thead>
-            <tr>
-              <th className="dsc-tb576">ID</th>
-              <th></th>
-              <th className="dsc-tb768">Preço</th>
-              <th className="dsc-txt-left">Nome</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr>
-                <td className="dsc-tb576">{product.id}</td>
-                <td>
-                  <img
-                    className="dsc-product-listing-image"
-                    src={product.imgUrl}
-                    alt={product.name}
-                  />
-                </td>
-                <td className="dsc-tb768">R$ {product.price.toFixed(2)}</td>
-                <td className="dsc-txt-left">{product.name}</td>
-                <td>
-                  <img
-                    className="dsc-product-listing-btn"
-                    src={editImg}
-                    alt="Editar"
-                  />
-                </td>
-                <td>
-                  <img
-                    className="dsc-product-listing-btn"
-                    src={deleteImg}
-                    alt="Deletar"
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {!isLastPage && (
-          <div onClick={handleNextPageClick} className="dsc-btn-next-page">
-            Carregar mais
-          </div>
-        )}
       </section>
     </main>
   );
